45585950640ab51064b3d2f1b5daede13ba51729
katshun0307 <shunten3@gmail.com>
2018-01-09 13:59:50 +0900

    completed synchorous sequential circut

--- task2/simulation/modelsim/gate_work/main/_primary.vhd
+++ task2/simulation/modelsim/gate_work/main/_primary.vhd
@@ -2,8 +2,9 @@ library verilog;
 use verilog.vl_types.all;
 entity main is
     port(
-        out1            : out    vl_logic_vector(15 downto 0);
+        out0            : out    vl_logic_vector(15 downto 0);
         \in\            : in     vl_logic_vector(15 downto 0);
+        out1            : out    vl_logic_vector(15 downto 0);
         reset           : in     vl_logic;
         clock           : in     vl_logic;
         opcode          : in     vl_logic_vector(3 downto 0);
