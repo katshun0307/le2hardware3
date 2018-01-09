f750f68d5d0a53a6af952b96bc2f799791ee4558
katshun0307 <shunten3@gmail.com>
2018-01-09 10:35:57 +0900

    updated task1 ALU

--- task1/simulation/modelsim/gate_work/main/_primary.vhd
+++ task1/simulation/modelsim/gate_work/main/_primary.vhd
@@ -2,9 +2,10 @@ library verilog;
 use verilog.vl_types.all;
 entity main is
     port(
-        mainout         : out    vl_logic_vector(15 downto 0);
+        cond            : out    vl_logic_vector(3 downto 0);
         opcode          : in     vl_logic_vector(3 downto 0);
         inputA          : in     vl_logic_vector(15 downto 0);
-        inputB          : in     vl_logic_vector(15 downto 0)
+        inputB          : in     vl_logic_vector(15 downto 0);
+        mainoutput      : out    vl_logic_vector(15 downto 0)
     );
 end main;
